8853cfdf0b61d88459dadacbff6b44aeb5edad41
xFusionTech <arout@xfusiontech.com>
2016-06-08 18:54:05 -0700

    Add files via upload

---
+++ Src/EMailService/Model/verification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroService.Model
+{
+    public class verification
+    {
+        public string type { get; set; }
+        public string to { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+}
