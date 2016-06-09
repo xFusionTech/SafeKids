58e10cdfb97aba79d6d023c0a3327e2ba65d77e8
xFusionTech <arout@xfusiontech.com>
2016-06-08 19:13:19 -0700

    Add files via upload

---
+++ Src/SignInService/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroService.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+
+        public User(int Id, string Name, string City)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.City = City;
+        }
+    }
+}
---
+++ Src/SignInService/OwinStartup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+[assembly: OwinStartup(typeof(SignInService.OwinStartup))]
+
+namespace SignInService
+{
+    public class OwinStartup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host.
+            HttpConfiguration config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+            config.Routes.MapHttpRoute(
+            name: "DefaultApi",
+            routeTemplate: "api/{controller}/{token}/{id}",
+            defaults: new { id = RouteParameter.Optional, token = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+            appBuilder.Run(sample =>
+            {
+                sample.Response.ContentType = "text/plain";
+                return sample.Response.WriteAsync("Hello from Signin Service");
+            });
+        }
+    }
+}
