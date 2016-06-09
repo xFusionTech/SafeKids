b4c5d3d1fdd27b7c2982c9197e79ca0e9ebe26be
xFusionTech <arout@xfusiontech.com>
2016-06-08 18:34:30 -0700

    Add files via upload

---
+++ Src/ADPQ.Business/Contracts/ChildBusinessContract.cs
@@ -0,0 +1,18 @@
+﻿using ADPQ.Entities.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADPQ.Business.Contracts
+{
+    public interface ChildBusinessContract
+    {
+        Guid Add(User child);
+        bool Edit(User child);
+        User Get(Guid childId,Guid personId);
+        List<RelationshipMap> GetChildList(Guid PERS_ID);
+        bool Delete(string filename, string Type);
+    }
+}
---
+++ Src/ADPQ.Business/Contracts/UserBusinessContract.cs
@@ -0,0 +1,23 @@
+﻿using ADPQ.Entities.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADPQ.Business.Contracts
+{
+    public interface UserBusinessContract
+    {
+        User login(User p);
+        User getUserData(Guid PERS_ID, Guid Token);
+        Guid Register(User NewUser);
+        User GetUserProfile(Guid PERS_ID);
+        bool UpdateProfile(User UpdateUser);
+     //   Guid CreateToken(Guid PERS_ID);
+        bool ValidateToken(Guid AuthToken);
+        Guid GetPER_ID(Guid Token);
+        bool CheckUserID(string Username, int type);
+    }
+  
+}
